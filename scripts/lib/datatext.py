9f88829a58b312893de5570e2595968854b2bbca
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-07-10 18:43:56 +0800

    add functions

--- scripts/lib/datatext.py
+++ scripts/lib/datatext.py
@@ -3,6 +3,10 @@ import argparse
 import os
 from nltk import *
 from termcolor import colored
+import re
+
+# import nltk
+# print dir(nltk)
 
 class DataText:
     def __init__(self):
@@ -20,6 +24,9 @@ class DataText:
         text infile.txt --div
         text infile.txt --common 4
         text infile.txt --common 10 --plot
+        text infile.txt --contains word
+        text infile.txt --find word1 word2
+        text infile.txt --similar word
         """)
         parser.add_argument(
             '-v',
@@ -57,9 +64,24 @@ class DataText:
             nargs="+",
             help="count words")
         parser.add_argument(
+            '--find',
+            nargs="+",
+            help="count words")
+        parser.add_argument(
+            '--context',
+            nargs="+",
+            help="count words")
+        parser.add_argument(
+            '--dispersion',
+            nargs="+",
+            help="show dispersion plot")
+        parser.add_argument(
             '--common',
             nargs=1,
             help="show common words")
+        parser.add_argument(
+            '--similar',
+            help="show similar words")
         parser.add_argument(
             '--plot',
             action="store_true",
@@ -78,7 +100,7 @@ class DataText:
 
         self.args = parser.parse_args()
         self.filename, self.ext = os.path.splitext(self.args.infile[0])
-        self.data = self.infile_convert(self.args.infile[0])
+        self.data, self.text, self.token = self.infile_convert(self.args.infile[0])
 
         # some fancy code here
 
@@ -112,21 +134,33 @@ class DataText:
             for i in sorted(set(long_words)):
                 print i
 
+        if self.args.find:
+            for i in self.args.find:
+                self.token.concordance(i)
+
         if self.args.contains:
-            # up = [x.upper() for x in set(self.data)]
-            # print sorted(x for x in up if self.args.contains[0] in x.upper())
-            # print [x for x in up if self.args.contains[0] in x.upper()]
             for i in [x for x in set(self.data) if self.args.contains[0].upper() in x.upper()]:
-                print colored(self.args.contains[0],"red").join(i.split(self.args.contains[0]))
+                # print re.split(self.args.contains[0],i, flags=re.IGNORECASE)
+                print colored(self.args.contains[0],"red").join(re.split(self.args.contains[0],i, flags=re.IGNORECASE))
+
+        if self.args.similar:
+            self.token.similar(self.args.similar)
+
+        if self.args.dispersion:
+            self.token.dispersion_plot(self.args.dispersion)
+
+        if self.args.context:
+            self.token.common_contexts(self.args.context)
 
         if self.args.show:
             print self.data
 
     def infile_convert (self, file_name):
-        text = "Lorem"
+        text = ""
+        temp = tokenize.WhitespaceTokenizer()
         try:
             with open (self.args.infile[0], "r") as myfile:
                 text = myfile.read()
         except:
             pass
-        return text.split()
+        return text.split(), text, Text(temp.tokenize(text))
