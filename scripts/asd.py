797602fc0cdfce8ef0086571948bfab0582b7a27
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-09-06 15:55:59 +0800

    clean gos

---
+++ scripts/asd.py
@@ -0,0 +1,9 @@
+qwe = "RE"
+asd = ("Lorem ipsum dolor " + qwe + " sit amet, \n"
+"consectetur adipisicing elit, \n"
+"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n"
+"Ut enim ad minim veniam, \n"
+"Excepteur sint occaecat cupidatat non proident, \n"
+"sunt in culpa qui officia deserunt mollit anim id est laborum.\n"
+)
+print asd
